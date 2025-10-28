e7aa49aa8fb0d835ab030f9a50e77412fc1685fe
keerthirajsivashankar <keerthirajsivashankar@gmail.com>
2025-10-28 15:03:00 +0530

    new stuff

--- dom/app.js
+++ dom/app.js
@@ -12,8 +12,8 @@ button.addEventListener("click", changeContent);
 // calculate area
 
 function calculateArea() {
-  let height = parsefloat(document.getElementById("h").value);
-  let width = parsefloat(document.getElementById("w").value);
+  let height = parseFloat(document.getElementById("h").value);
+  let width = parseFloat(document.getElementById("w").value);
   let area = height * width;
   let result = document.getElementById("result");
   result.innerHTML = "The area is :" + area;
@@ -21,3 +21,37 @@ function calculateArea() {
 
 let calcButton = document.getElementById("calculate");
 calcButton.addEventListener("click", calculateArea);
+
+// create shopping list
+console.log("Creating shopping list...");
+let shoppinglist = ["milk", "eggs", "bread", "butter", "cheese"];
+
+let parent = document.getElementById("shopping");
+
+let n = shoppinglist.length;
+
+for (let i = 0; i < n; i++) {
+  let curr = shoppinglist[i];
+
+  let newele = document.createElement("li");
+  newele.innerHTML = curr;
+  parent.appendChild(newele);
+}
+
+// to-do list functionality
+function addTask() {
+  let deletebtn = document.createElement("button");
+  deletebtn.innerHTML = " X";
+  let taskInput = document.getElementById("task").value;
+  let newli = document.createElement("li");
+  newli.innerHTML = taskInput;
+  newli.appendChild(deletebtn);
+  deletebtn.addEventListener("click", function () {
+    todoList.removeChild(newli);
+  });
+  todoList.appendChild(newli);
+  document.getElementById("task").value = "";
+}
+let todoList = document.getElementById("todoList");
+let addbuttontask = document.getElementById("addTask");
+addbuttontask.addEventListener("click", addTask);
